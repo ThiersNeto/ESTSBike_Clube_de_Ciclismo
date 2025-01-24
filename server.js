d5dd893feba85dd9fff36fe4b9183828749a3e00
AndreRochaEST <202300185@estudantes.ips.pt>
2025-01-24 19:11:12 +0000

    correção routes

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import bodyParser  from 'body-parser';
-import eventTypeRoutes from './Server/Routes/eventTypeRoutes.js';
-import eventRoutes  from './Server/Routes/eventRoutes.js';
-import memberRoutes  from './Server/Routes/memberRoutes.js';
+import eventTypeController from './Server/Controllers/EventTypeController.js';
+import eventController  from './Server/Controllers/EventController.js';
+import memberController  from './Server/Controllers/MemberController.js';
 
 const app = express();
 
@@ -23,6 +23,41 @@ app.use((req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Algo deu errado!');
   });
+
+// Rotas para gerir eventos
+router.get('/event', eventController.getAllEvents);
+router.get('/:id', eventController.getEvent);
+router.post('/', eventController.createEvent);
+router.put('/:id', eventController.updateEvent);
+router.delete('/:id', eventController.deleteEvent);
+
+// Rotas adicionais para gerir inscrições em eventos
+router.post('/:id/subscribe', eventController.subscribeToEvent);
+router.delete('/:id/unsubscribe', eventController.unsubscribeFromEvent);
+
+router.get('/:id/members', eventController.getEventMembers);
+
+// Rotas para gerir tipos de eventos
+router.get('/', eventTypeController.getAllEventTypes);
+router.get('/:id', eventTypeController.getEventType);
+router.post('/', eventTypeController.createEventType);
+router.put('/:id', eventTypeController.updateEventType);
+router.delete('/:id', eventTypeController.deleteEventType);
+
+// Rotas para gerir membros
+router.get('/', memberController.getAllMembers);
+router.get('/:id', memberController.getMember);
+router.post('/', memberController.createMember);
+router.put('/:id', memberController.updateMember);
+router.delete('/:id', memberController.deleteMember);
+
+// Rotas adicionais para gerir preferências de tipos de eventos dos membros
+router.get('/:id/preferences', memberController.getMemberPreferences);
+router.post('/:id/preferences', memberController.addMemberPreference);
+router.delete('/:id/preferences/:eventTypeId', memberController.removeMemberPreference);
+
+// rota para adicionar um membro a um evento
+router.post('/:id/events', memberController.addMemberEvent);
 
 app.use('/api/event-types', eventTypeRoutes);
 app.use('/api/events', eventRoutes);
